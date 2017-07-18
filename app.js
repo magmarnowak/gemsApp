363679eb59e234764634b99397746cd16e0a6e47
magmarnowak <magmarnowak@gmail.com>
2017-07-18 14:42:25 +0200

    Images, cudtom directives, gallery controller

--- app.js
+++ app.js
@@ -5,13 +5,11 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function(){
-    this.tab = 1;
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
+  app.controller('GalleryController', function() {
+    this.imageIndex = 0;
+    this.currentImageChange = function(imageNumber) {
+      console.log(imageNumber);
+      this.imageIndex = imageNumber || 0;
     };
   });
 
@@ -19,52 +17,112 @@
     this.review = {};
 
     this.addReview = function(product) {
-      this.review = {
-        createdOn: Date.now(),
-      };
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
   });
 
-  var gems = [
-    {
-      name: 'Dodecahedron',
-      price: 2.95,
-      description: '...',
-      images:[
-        {
-          full: 'img/gem-01.gif',
-          thumb: 'img/gem-02.gif'
-        }
-      ],
-      reviews: [
-        {
-          stars: 5,
-          boy: "I love this product!",
-          author: "joe@thomas.com"
-        },
-        {
-          stars: 1,
-          body: "This product sucks",
-          author: "tim@hater.com"
-        }
-      ],
-      canPurchase: true,
-      soldOut: true,
-    },
-    {
-      name: 'Pentagonal Gem',
-      price: 5.95,
-      description: '...',
-      images:[
-        {
-          full: 'img/gem-03.gif',
-          thumb: 'img/gem-04.gif'
-        }
-      ],
-      canPurchase: true,
-      soldOut: false,
-    }
-  ];
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html',
+    };
+  });
+
+  app.directive("productPanels", function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: ('PanelController', function(){
+        this.tab = 1;
+        this.selectTab = function(setTab){
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        };
+      }),
+      controllerAs: 'panel',
+    };
+  });
+
+  var gems = [{
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [
+      "img/gem-02.gif",
+      "img/gem-05.gif",
+      "img/gem-09.gif"
+    ],
+    reviews: [{
+      stars: 5,
+      body: "I love this gem!",
+      author: "joe@thomas.com",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "This gem sucks.",
+      author: "tim@hater.com",
+      createdOn: 1397490980837
+    }]
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
+    images: [
+      "img/gem-01.gif",
+      "img/gem-03.gif",
+      "img/gem-04.gif",
+    ],
+    reviews: [{
+      stars: 3,
+      body: "I think this gem was just OK, could honestly use more shine, IMO.",
+      author: "JimmyDean@sausage.com",
+      createdOn: 1397490980837
+    }, {
+      stars: 4,
+      body: "Any gem with 12 faces is for me!",
+      author: "gemsRock@alyssaNicoll.com",
+      createdOn: 1397490980837
+    }]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "img/gem-06.gif",
+      "img/gem-07.gif",
+      "img/gem-10.gif"
+    ],
+    reviews: [{
+      stars: 1,
+      body: "This gem is WAY too expensive for its rarity value.",
+      author: "turtleguyy@zdn.me",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "BBW: High Shine != High Quality.",
+      author: "LouisW407@gmail.com",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "Don't waste your rubles!",
+      author: "nat@flatland.com",
+      createdOn: 1397490980837
+    }]
+  }];
 })();
